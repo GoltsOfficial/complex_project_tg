a31d0caeba54dd543e7f69685a0a41119fe82b0d
GoltsOfficial <goltshteyngottorp@gmail.com>
2025-09-30 07:03:39 +0300

    Update loader.py

--- ad_bot/loader.py
+++ ad_bot/loader.py
@@ -1,12 +1,13 @@
-# loader.py
+# ad_bot/loader.py
 import sys
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from config import TOKEN
+from .config import TOKEN  # относительный импорт
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -15,3 +16,4 @@ bot: Bot = Bot(
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 dp: Dispatcher = Dispatcher()
+
